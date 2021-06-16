3d1de8a61b4e25a1a39a49cb018320ad9c9586db
evargas1 <73708855+evargas1@users.noreply.github.com>
2021-06-15 19:12:49 -0500

    added the database

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'image', 'link_git', 'link_view')
+    list_filter = ('title', )
+    # ordering = ('title', )
+    search_field = ('title')
+    
+admin.site.register(models.Project, ProjectAdmin)
+
+
+# testing my commits
+# this will make sure i can view my data table on admin page 
+# this will also make adding items to my database a lot easier
--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+# this is setting up my main database
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=40)
+    description = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='gallery')
+    link_git = models.URLField(max_length=200, null=True)
+    link_view = models.URLField(max_length=200, null=True)
+    
