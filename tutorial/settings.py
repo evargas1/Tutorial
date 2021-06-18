d5f53437a796af1816dbc52a1f5a60568d0f1512
evargas1 <73708855+evargas1@users.noreply.github.com>
2021-06-17 21:18:18 -0500

    added to settings

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -128,8 +128,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = '/tutorial/site/public/static'
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# MEDIA_URL = '/gallery/'
-# MEDIA_ROOT =  '/tutorial/site/public/static/gallery/'
+MEDIA_URL = '/gallery/'
+MEDIA_ROOT =  '/tutorial/site/public/gallery/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
