df652fb157f562c1852b6d80f6d74fe0d551b169
evargas1 <73708855+evargas1@users.noreply.github.com>
2021-06-18 15:34:31 -0500

    added too

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -135,3 +135,6 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# /tutorial/django/tutorial/tutorial/
